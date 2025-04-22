e3904cba50b8438fc59f2ee8529b3f7b3bfdab78
ali kadhim <ali20140022@gmail.com>
2025-04-22 21:10:40 +0300

    add assets and make a header

--- app/admin/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Admin() {
-  return (
-    <>
-      <div className="container bg-red-500 w-full mx-auto text-white px-2 py-4">
-        <h1>admin page</h1>
-      </div>
-    </>
-  );
-}
---
+++ app/home/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Header from "../_components/Header";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <Button variant={"outline"}>click me now</Button>
+    </div>
+  );
+}
