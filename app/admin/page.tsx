3719c5113b9861a64e1d629762e20a988873ae7e
ali kadhim <ali20140022@gmail.com>
2025-04-22 20:07:32 +0300

    first commit

---
+++ app/admin/page.tsx
@@ -0,0 +1,9 @@
+export default function Admin() {
+  return (
+    <>
+      <div className="container bg-red-500 w-full mx-auto text-white px-2 py-4">
+        <h1>admin page</h1>
+      </div>
+    </>
+  );
+}
